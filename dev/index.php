60605931fe3933d6ff1f0edf67ce9e6a729d634c
Josh Broton <joshbroton@gmail.com>
2015-02-17 23:06:59 -0600

    class name fix

--- dev/index.php
+++ dev/index.php
@@ -1,6 +1,6 @@
 <?php get_template_part('templates/header'); ?>
 
-<section class="content">
+<section class="content_wrapper">
     <main class="content" role="main">
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?> role="article">
